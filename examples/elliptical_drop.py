310cf8ddee5338db31714cfda4720028da3ed889
Kunal Puri <none@none>
2013-03-29 13:16:13 +0530

    ENH: Adding an exact solution to the elliptical drop test case.

--- examples/elliptical_drop.py
+++ examples/elliptical_drop.py
@@ -93,3 +93,31 @@ integrator = WCSPHRK2Integrator(evaluator=evaluator, particles=particles)
 
 # f = evaluator.calc.fluid
 # print type(f), f, f.x
+
+def exact_solution(tf=0.00076, dt=1e-4):
+    """ Exact solution for the the elliptical drop equations """
+    import numpy
+    
+    A0 = 100
+    a0 = 1.0
+
+    t = 0.0
+
+    theta = numpy.linspace(0,2*numpy.pi, 101)
+
+    Anew = A0
+    anew = a0
+
+    while t <= tf:
+        t += dt
+
+        Aold = Anew
+        aold = anew
+        
+        Anew = Aold +  dt*(Aold*Aold*(aold**4 - 1))/(aold**4 + 1)
+        anew = aold +  dt*(-aold * Aold)
+
+    dadt = Anew**2 * (anew**4 - 1)/(anew**4 + 1)
+    po = 0.5*-anew**2 * (dadt - Anew**2)
+        
+    return anew*numpy.cos(theta), 1/anew*numpy.sin(theta), po
