a56ee786727a05d7b818125a5e86c75f2bcb7bc8
kunalp <none@none>
2013-05-07 14:50:42 +0530

    ENH: Updating locators to require an unsigned int array for neighbors
    --HG--
    branch : upstream https://kunalp:252512EM@bitbucket.org/kunalp/pysph
    head https://kunalp:252512em@bitbucket.org/prabhuramachandran/pysph

--- pysph/base/locators.py
+++ pysph/base/locators.py
@@ -18,18 +18,18 @@ class AllPairLocator(Locator):
         cdef class AllPairLocator:
             cdef ParticleArrayWrapper src, dest
             cdef long N
-            cdef LongArray nbrs
+            cdef UIntArray nbrs
             def __init__(self, ParticleArrayWrapper src, 
                          ParticleArrayWrapper dest):
                 self.src = src
                 self.dest = dest
                 self.N = src.size()
-                self.nbrs = LongArray(self.N)
+                self.nbrs = UIntArray(self.N)
                 cdef long i
                 for i in range(self.N):
                     self.nbrs[i] = i
                 
-            def get_neighbors(self, long d_idx, LongArray nbr_array):
+            def get_neighbors(self, long d_idx, UIntArray nbr_array):
                 nbr_array.resize(self.N)
                 nbr_array.set_data(self.nbrs.get_npy_array())
         '''
