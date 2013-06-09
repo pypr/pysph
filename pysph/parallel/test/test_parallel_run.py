e8cce589a57cda7484c54279b1ae9c14eb668b36
kunalp <none@none>
2013-06-09 12:12:33 +0200

    ENH: Adding the test for the parallel examples.
    The test is borrowed from the old PySPH wherein an example is run in serial and
    then in parallel and compared for results.

---
+++ pysph/parallel/test/test_parallel_run.py
@@ -0,0 +1,193 @@
+""" Module to run the example files and report their success/failure results
+
+Add a function to the ExampleTest class corresponding to an example script to
+be tested.
+This is done till better strategy for parallel testing is implemented
+
+"""
+
+try:
+    import mpi4py.MPI as mpi
+except ImportError:
+    import nose.plugins.skip as skip
+    reason = "mpi4py not installed"
+    raise skip.SkipTest(reason)
+
+import unittest
+from subprocess import Popen, PIPE
+from threading import Timer
+import os
+import sys
+import tempfile
+import shutil
+import numpy
+
+from nose.plugins.attrib import attr
+
+from pysph.solver.utils import load
+
+directory = os.path.dirname(os.path.abspath(__file__))
+
+def kill_process(process):
+    print 'KILLING PROCESS ON TIMEOUT'
+    process.kill()
+
+def get_files(fname):
+
+    files = os.listdir( '.' )
+    path = os.path.abspath( '.' )
+
+    # get all the output files in the directory
+    files = [f for f in files if f.startswith(fname) and f.endswith('.npz') ]
+    files = [os.path.join(path, f) for f in files]
+
+    # sort the files
+    def _sort_func(x, y):
+        """Sort the files correctly."""
+        def _process(arg):
+            a = os.path.splitext(arg)[0]
+            return int(a[a.rfind('_')+1:])
+        return cmp(_process(x), _process(y))
+
+    files.sort(_sort_func)
+
+    return files    
+
+def _run_example_script(filename, args=[], nprocs=2, timeout=20.0, path=None):
+    """ run a file python script
+    
+    Parameters:
+    -----------
+    filename - filename of python script to run under mpi
+    nprocs - (2) number of processes of the script to run (0 => serial non-mpi run)
+    timeout - (5) time in seconds to wait for the script to finish running,
+        else raise a RuntimeError exception
+    path - the path under which the script is located
+        Defaults to the location of this file (__file__), not curdir
+    
+    """
+    path = directory
+    path = os.path.join(path, filename)
+    cmd = ['mpiexec','-n', str(nprocs), sys.executable, path] + args
+
+    print 'running test:', cmd
+
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    timer = Timer(timeout, kill_process, [process])
+    timer.start()
+    out, err = process.communicate()
+    timer.cancel()
+    retcode = process.returncode
+    if retcode:
+        msg = 'test ' + filename + ' failed with returncode ' + str(retcode)
+        print out
+        print err
+        print '#'*80
+        print msg
+        print '#'*80
+        raise RuntimeError, msg
+    return retcode, out, err
+
+class ExampleTestCase(unittest.TestCase):
+    """ A script to run an example in serial and parallel and compare results.
+
+    To test an example in parallel, subclass from ExampleTest and
+    write a test function like so:
+
+    def test_elliptical_drop(self):
+        self.run_example('../../../../examples/elliptical_drop.py',
+                         timestep=1e-5, iters=100, nprocs=2, timeout=60)
+    
+    """
+    def run_example(self, filename, tf=0.01, nprocs=2, load_func=load,
+                    timeout=300):
+        """Run an example and compare the results in serial and parallel.
+
+        Parameters:
+        -----------
+
+        filename : str
+            The name of the file to run
+
+        timestep : double
+            The time step argument to pass to the example script
+
+        iters : double
+            The number of iteations to evolve the example
+
+        nprocs : int
+            Number of processors to use for the example.
+
+        timeout : int
+            Time in seconds to wait for execution before an error is raised.
+
+        path : Not used
+
+        """
+        prefix = os.path.splitext(os.path.basename(filename))[0]
+        
+        try:
+            # dir1 is for the serial run
+            dir1 = tempfile.mkdtemp()
+
+            # dir2 is for the parallel run 
+            dir2 = tempfile.mkdtemp()
+
+            args = ['--fname=%s'%prefix,
+                    '--directory=%s'%dir1,
+                    '--tf=%g'%(tf),
+                    '--pfreq=1000000']
+
+            # run the example script in serial
+            _run_example_script(filename, args, 1, timeout)
+
+            # run the example script in parallel
+            args[1] = '--directory=%s'%dir2
+            _run_example_script(filename, args, nprocs, timeout)
+
+            # get the serial and parallel results
+            os.chdir( dir1 )
+            file = get_files(prefix)[-1]
+            serial = load(file)
+
+            serial = serial['arrays']['fluid']
+
+            os.chdir( dir2 )
+            file = get_files(prefix)[-1]
+            parallel = load(file)
+
+            parallel = parallel['arrays']['fluid']
+            
+        finally:
+            shutil.rmtree(dir1, True)
+            shutil.rmtree(dir2, True)
+
+        # test
+        self._test(serial, parallel)
+
+    def _test(self, serial, parallel):
+        # make sure the arrays are at the same time
+        self.assertAlmostEqual( serial.time, parallel.time, 8)
+        
+        # test the results.
+        xs, ys, zs, rhos  = serial.get("x","y", "z", "rho")
+        xp, yp, zp, rhop, gid = parallel.get("x", "y", "z", "rho", 'gid')
+
+        self.assertTrue( xs.size,xp.size )
+        np = xs.size
+
+        places = 10
+        for i in range(np):
+            self.assertAlmostEqual( xs[ gid[i] ], xp[i], places )
+            self.assertAlmostEqual( ys[ gid[i] ], yp[i], places )
+            self.assertAlmostEqual( zs[ gid[i] ], zp[i], places )
+            self.assertAlmostEqual( rhos[ gid[i] ], rhop[i], places )
+
+class DamBreakTestCase(ExampleTestCase):
+
+    @attr(slow=True, parallel=True)
+    def test_db(self):
+        self.run_example('../../../examples/dam_break.py', nprocs=4, load_func=load, tf=0.01)
+
+if __name__ == "__main__":
+    unittest.main()
