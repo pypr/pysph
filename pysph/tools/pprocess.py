1a8f432f7e7c5398f77345d1621149fb0e110a91
Kunal Puri <kunal.r.puri@gmail.com>
2013-07-04 12:44:13 +0530

    ENH: Adding a utility to dump out vtk files for ParaView

--- pysph/tools/pprocess.py
+++ pysph/tools/pprocess.py
@@ -6,6 +6,16 @@ try:
 except ImportError:
     VV=False
 
+TVTK=True
+try:
+    from tvtk.api import tvtk, write_data
+except ImportError:
+    TVTK = False
+
+if TVTK:
+    from tvtk.array_handler import array2vtk
+
+from os import path
 import numpy as np
 import pysph.solver.utils as utils
 
@@ -46,3 +56,58 @@ class Results(object):
             
             m, u, v, w = array.get('m', 'u', 'v', 'w')
             ke[i] = 0.5 * np.sum( m * (u**2 + v**2) )
+
+    def _write_vtk_snapshot(self, mesh, directory, _fname):
+        fname = path.join(directory, _fname)
+        write_data( mesh, fname )
+
+    def write_vtk(self, array_name, props):
+        if not TVTK:
+            return
+
+        # create a list of props
+        if type(props) != list:
+            props = [ props ]
+
+        # create an output folder for the vtk files
+        dirname = path.join(self.dirname, 'vtk')
+        utils.mkdir(dirname)
+        
+        nfiles = self.nfiles
+        for i in range(nfiles):
+            f = self.files[i]
+            data = utils.load(f)
+
+            array = data['arrays'][array_name]
+            num_particles = array.num_real_particles
+            
+            # save the points
+            points = np.zeros( shape=(num_particles,3) )
+            points[:, 0] = array.z
+            points[:, 1] = array.y
+            points[:, 2] = array.x
+            
+            mesh = tvtk.PolyData(points=points)
+
+            # add the scalar props
+            for prop in props:
+                if prop == 'vmag':
+                    u, v, w = array.get('u','v','w')
+                    numpy_array = np.sqrt(u**2 + v**2 + w**2)
+                else:
+                    numpy_array = array.get(prop)
+                
+                vtkarray = array2vtk(numpy_array)
+                vtkarray.SetName(prop)
+            
+                # add the array as point data
+                mesh.point_data.add_array(vtkarray)
+                
+            # set the last prop as the active scalar
+            mesh.point_data.set_active_scalars(props[-1])
+
+            # spit it out
+            fileno = data['solver_data']['count']
+            _fname = self.fname + '_%s_%s'%(array_name, fileno)
+            
+            self._write_vtk_snapshot(mesh, dirname, _fname)
