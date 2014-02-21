cbf4e8f4e175108e6d705e0d97ec87a906d2539f
Kunal Puri <kunal.r.puri@gmail.com>
2014-02-22 04:26:53 +0530

    FIX: Fixing MPI flags when Zoltan is not installed
    PyZoltan will be installed by default for CArrays. To check for
    parallel Zoltan, we need to check for 'import pyzoltan.core.zoltan'
    instead of 'import pyzoltan'

--- pysph/__init__.py
+++ pysph/__init__.py
@@ -1,13 +1,17 @@
 # Conditional Imports for Parallel stuff
 
+# Check for MPI
 Has_MPI=True
 try:
     import mpi4py
 except ImportError:
     Has_MPI=False
 
+# Check for Zoltan
 Has_Zoltan=True
 try:
-    import pyzoltan
+    from pyzoltan.core import Zoltan
 except ImportError:
+    # We switch off parallel mode
     Has_Zoltan=False
+    Has_MPI=False
