4515f10c731ae7a64297d316f344748405f395ad
Kunal Puri <kunal.r.puri@gmail.com>
2014-06-17 12:31:26 +0530

    DOC: Updating docstrings for the transport velocity equations

--- pysph/sph/wc/transport_velocity.py
+++ pysph/sph/wc/transport_velocity.py
@@ -1,9 +1,39 @@
-"""Functions for the transport velocity formulation of Adami et. al.
+"""Implementation of the Transport Velocity Formulation with
+generalized wall boundary conditions of Adami et. al
+
+The references are
+
+ - Adami et. al "A generalized wall boundary condition for smoothed
+   particle hydrodynamics", 2012, JCP, 7057--7075 (REF1)
+
+ - Adami et. al "A transport-velocity formulation for smoothed
+   particle hydrodynamics", 2013, JCP, 292--307 (REF2)
+
 """
 
 from pysph.sph.equation import Equation
 
 class DensitySummation(Equation):
+    """Summation density with volume summation
+
+    In addition to the standard summation density, the number density
+    for the particle is also computed. The number density is important
+    for multi-phase flows to define a local particle volume
+    independent of the material density.
+
+    Notes:
+    
+    The volume for the particle, computed from the number density is
+    given as:
+
+    .. math::
+
+        \mathcal{V}_a = \frac{1}{\sum_b W_{ab}}
+
+    For this equation, the destination particle array must define the
+    variable `V` for particle volume.
+
+    """
     def initialize(self, d_idx, d_V, d_rho):
         d_V[d_idx] = 0.0
         d_rho[d_idx] = 0.0
@@ -13,6 +43,11 @@ class DensitySummation(Equation):
         d_rho[d_idx] += d_m[d_idx]*WIJ
 
 class VolumeSummation(Equation):
+    """Number density for volume computation.
+
+    See `DensitySummation`
+
+    """
     def initialize(self, d_idx, d_V):
         d_V[d_idx] = 0.0
 
@@ -20,6 +55,14 @@ class VolumeSummation(Equation):
         d_V[d_idx] += WIJ
 
 class ContinuityEquation(Equation):
+    """Conservation of mass equation Eq (6) in REF1
+
+    .. math::
+
+        \frac{d\rho_a}{dt} = \rho_a \sum_b \frac{m_b}{\rho_b}
+        \boldsymbol{v}_{ab} \cdot \nabla_a W_{ab}
+
+    """
     def initialize(self, d_idx, d_arho):
         d_arho[d_idx] = 0.0
 
@@ -29,6 +72,27 @@ class ContinuityEquation(Equation):
         d_arho[d_idx] += d_m[d_idx] * Vj * vijdotdwij
 
 class StateEquation(Equation):
+    """Generalized weakly compressible EOS:
+
+    .. math::
+
+        p_a = p_0\left[ \left(\frac{\rho}{\rho_0}\right)^\gamma - b
+        \right] + \mathcal{X}
+
+     Notes:
+     
+     This is the generalized Tait's equation of state and the
+     suggested values in REF2 are :math:`\mathcal{X} = 0`,
+     :math:`\gamma=1` and :math:`b = 1`
+
+     The reference pressure :math:`p_0` is calculated from the
+     artificial sound speed and reference density:
+     
+     .. math::
+      
+         p_0 = \frac{c^2\rho_0}{\gamma}
+
+    """
     def __init__(self, dest, sources=None, p0=1.0, rho0=1.0, b=1.0):
         self.b=b
         self.p0 = p0
@@ -39,6 +103,59 @@ class StateEquation(Equation):
         d_p[d_idx] = self.p0 * ( d_rho[d_idx]/self.rho0 - self.b )
 
 class SolidWallBC(Equation):
+    """Solid wall boundary condition described in REF1
+    
+    This boundary condition is to be used with fixed ghost particles
+    in SPH simulations and is formulated for the general case of
+    moving boundaries.
+
+    The velocity and pressure of the fluid particles is extrapolated
+    to the ghost particles and these values are used in the equations
+    of motion.
+
+    No-penetration:
+    
+    Ghost particles participate in the continuity and state equations
+    with fluid particles. This means as fluid particles approach the
+    wall, the pressure of the ghost particles increases to generate a
+    repulsion force that prevents particle penetration.
+
+    No-slip:
+    
+    Extrapolation is used to set the `dummy` velocity of the ghost
+    particles for viscous interaction. First, a Shepard filtered
+    velocity is computed for fluid particles in the vicinity of the
+    wall:
+
+    .. math::
+    
+        \tilde{v}_a = \frac{\sum_b v_b W_{ab}}{\sum_b W_{ab}}
+
+    In the second step, for the viscous interaction in Eqs. (10) in
+    REF1 and Eq. (8) in REF2, the velocity of the ghost particles is
+    temorarily assigned as:
+
+    .. math::
+
+       v_b = 2v_w -\tilde{v}_a,
+
+    where :math:`v_w` is the prescribed wall velocity and :math:`v_b`
+    is the ghost particle in the interaction.
+
+    Pressure boundary condition:
+    
+    The pressure of the ghost particle is also calculated from the
+    fluid particle by interpolation using:
+
+    .. math::
+
+        p_g = \frac{\sum_f p_f W_{gf} + \boldsymbol{g - a_g} \cdot
+        \sum_f \rho_f \boldsymbol{r}_{gf}W_{gf}}{\sum_f W_{gf}},
+
+    where the subscripts `g` and `f` relate to the ghost and fluid
+    particles respectively.
+    
+    """
     def __init__(self, dest, sources=None, rho0=1.0, p0=100.0,
                  gx=0.0, gy=0.0, gz=0.0, ax=0.0, ay=0.0, az=0.0,
                  b=1.0):
@@ -89,6 +206,10 @@ class SolidWallBC(Equation):
         d_rho[d_idx] = self.rho0 * (d_p[d_idx]/self.p0 + self.b)
 
 class MomentumEquation(Equation):
+    """Momentum equation for the Transport Velocity formulation
+    Eq. (8) in REF2
+
+    """
     def __init__(self, dest, sources=None, pb=0.0, nu=0.01, gx=0.0, gy=0.0, gz=0.0):
         self.gx = gx
         self.gy = gy
