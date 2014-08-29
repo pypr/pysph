cedc6bf80a459490498a3f4a1b927aca8ccb827a
Kunal Puri <kunal.r.puri@gmail.com>
2014-08-29 14:51:22 +0530

    ENH: Adding density and smoothing length rate equations in the predictor for gasD

--- pysph/sph/integrator_step.py
+++ pysph/sph/integrator_step.py
@@ -348,7 +348,7 @@ class GasDFluidStep(IntegratorStep):
     """Predictor Corrector integrator for Gas-dynamics"""
     def initialize(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_h,
                    d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e, d_e0, d_h0,
-                   d_converged, d_omega):
+                   d_converged, d_omega, d_rho, d_rho0):
         d_x0[d_idx] = d_x[d_idx]
         d_y0[d_idx] = d_y[d_idx]
         d_z0[d_idx] = d_z[d_idx]
@@ -360,6 +360,7 @@ class GasDFluidStep(IntegratorStep):
         d_e0[d_idx] = d_e[d_idx]
 
         d_h0[d_idx] = d_h[d_idx]
+        d_rho0[d_idx] = d_rho[d_idx]
 
         # set the converged attribute to 0 at the beginning of a Group
         d_converged[d_idx] = 0
@@ -370,7 +371,7 @@ class GasDFluidStep(IntegratorStep):
 
     def stage1(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z,
                   d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e0, d_e, d_au, d_av,
-                  d_aw, d_ae, dt=0.0):
+                  d_aw, d_ae, d_rho, d_rho0, d_arho, d_h, d_h0, d_ah, dt=0.0):
         dtb2 = 0.5*dt
 
         d_u[d_idx] = d_u0[d_idx] + dtb2 * d_au[d_idx]
@@ -383,6 +384,12 @@ class GasDFluidStep(IntegratorStep):
 
         # update thermal energy
         d_e[d_idx] = d_e0[d_idx] + dtb2 * d_ae[d_idx]
+
+        # predict density and smoothing lengths for faster
+        # convergence. NNPS need not be explicitly updated since it
+        # will be called at the end of the predictor stage.
+        d_h[d_idx] = d_h0[d_idx] + dtb2 * d_ah[d_idx]
+        d_rho[d_idx] = d_rho0[d_idx] + dtb2 * d_arho[d_idx]
 
     def stage2(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z,
                    d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e0, d_e, d_au, d_av,
