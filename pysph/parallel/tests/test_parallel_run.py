161538b492df290725921e72c875cf22ce86e84d
Prabhu Ramachandran <prabhu@enthought.com>
2015-05-02 21:25:10 +0530

    Tests should be self-contained.
    The parallel example tests were running examples inside the examples
    directory which will not be available when pysph is installed as a
    package.  Instead we bundle the scripts to run in the tests.

--- pysph/parallel/tests/test_parallel_run.py
+++ pysph/parallel/tests/test_parallel_run.py
@@ -6,8 +6,6 @@ This is done till better strategy for parallel testing is implemented
 
 """
 
-import os
-from os.path import abspath, join
 import unittest
 
 from nose.plugins.attrib import attr
@@ -16,10 +14,6 @@ from pysph.tools import run_parallel_script
 from example_test_case import ExampleTestCase
 
 run_parallel_script.skip_if_no_mpi4py()
-
-MY_DIR = run_parallel_script.get_directory(__file__)
-
-EXAMPLE_DIR = abspath(join(MY_DIR, os.pardir, os.pardir, os.pardir, 'examples'))
 
 
 class ParallelTests(ExampleTestCase):
@@ -30,19 +24,7 @@ class ParallelTests(ExampleTestCase):
         serial_kwargs = dict(timestep=dt, tf=tf)
         extra_parallel_kwargs = dict(ghost_layers=1, lb_freq=5)
         self.run_example(
-            'dambreak3D.py', nprocs=4, timeout=900, atol=1e-4,
-            serial_kwargs=serial_kwargs,
-            extra_parallel_kwargs=extra_parallel_kwargs
-        )
-
-    @attr(slow=True, parallel=True)
-    def test_2Ddam_break_example(self):
-        dt = 1e-4; tf = 200*dt
-        serial_kwargs = dict(timestep=dt, tf=tf)
-        extra_parallel_kwargs = dict(ghost_layers=1, lb_freq=5)
-        script = join(EXAMPLE_DIR, 'dam_break.py')
-        self.run_example(
-            script, nprocs=4, atol=1e-4,
+            'dambreak3D.py', nprocs=4, atol=1e-4,
             serial_kwargs=serial_kwargs,
             extra_parallel_kwargs=extra_parallel_kwargs
         )
@@ -52,9 +34,8 @@ class ParallelTests(ExampleTestCase):
         dt=1e-4; tf=200*dt
         serial_kwargs = dict(timestep=dt, tf=tf)
         extra_parallel_kwargs = dict(ghost_layers=3, lb_freq=5)
-        script = join(EXAMPLE_DIR, 'transport_velocity', 'cavity.py')
         self.run_example(
-            script, nprocs=4, atol=1e-4, serial_kwargs=serial_kwargs,
+            'cavity.py', nprocs=4, atol=1e-4, serial_kwargs=serial_kwargs,
             extra_parallel_kwargs=extra_parallel_kwargs
         )
 
