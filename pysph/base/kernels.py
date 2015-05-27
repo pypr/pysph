8c5fc97b1ad8b027064aecf507f9b41b0a8ffe91
Arpit <arpit.r.agarwal@gmail.com>
2015-05-27 13:20:35 +0530

    Updating documentation of kernels.py
    Have added the function definitions for all kernels but the
    WendlandQuinticSpline.
    Have not added the kernel derivative as they are trivial to
    evaluate.

--- pysph/base/kernels.py
+++ pysph/base/kernels.py
@@ -1,3 +1,6 @@
+"""Definition of some SPH kernel functions
+"""
+
 from math import pi, sqrt, exp
 
 M_1_PI = 1.0/pi
@@ -20,6 +23,28 @@ def get_compiled_kernel(kernel):
 # `CubicSpline` class.
 ###############################################################################
 class CubicSpline(object):
+    r"""Cubic Spline Kernel: [1]_
+
+    .. math::
+
+             W(q) = \ &\sigma_3\left[ (2-q)^3 - 4(1-q)^3\right], \ & \textrm{for} \ 0 \leq q \leq 1,\\
+                  = \ &\sigma_3(2-q)^3, & \textrm{for}\ 1 \leq q \leq 2,\\
+                  = \ &0, & \textrm{for}\ q>2, \\
+
+    where :math:`\sigma_3` is a dimensional normalizing factor for the
+    cubic spline function given by:
+
+    .. math::
+             \sigma_3  = \ & \frac{2}{3h^1}, & \textrm{for dim=1}, \\
+             \sigma_3  = \ & \frac{10}{7\pi h^2}, \ & \textrm{for dim=2}, \\
+             \sigma_3  = \ & \frac{1}{\pi h^3}, & \textrm{for dim=3}. \\
+
+    References
+    ----------
+    .. [1] `Monaghan, J. J. (1992). Smoothed particle hydrodynamics.
+        Annual review of astronomy and astrophysics, 30, 543-574
+        <http://adsabs.harvard.edu/abs/1992ARA&A..30..543M>`_.
+    """
     def __init__(self, dim=1):
         self.radius_scale = 2.0
         self.dim = dim
@@ -192,6 +217,28 @@ class WendlandQuintic(object):
         return -fac * h1 * ( dw*q + w*self.dim )
 
 class Gaussian(object):
+    r"""Gaussian Kernel: [2]_
+
+    .. math::
+
+             W(q) = \ &\sigma_g e^{-q^2}, \ & \textrm{for} \ 0\leq q \leq 3,\\
+                  = \ & 0, & \textrm{for} \ q>3,\\
+
+    where :math:`\sigma_g` is a dimensional normalizing factor for the gaussian function
+    given by:
+
+    .. math::
+             \sigma_g  = \ & \frac{1}{\pi^{1/2} h^1}, \ & \textrm{for dim=1}, \\
+             \sigma_g  = \ & \frac{1}{\pi h^2}, \ & \textrm{for dim=2}, \\
+             \sigma_g  = \ & \frac{1}{\pi^{3/2} h^3}, & \textrm{for dim=3}. \\
+
+    References
+    ----------
+    .. [2] `Liu, M. B., & Liu, G. R. (2010). Smoothed particle
+        hydrodynamics (SPH): an overview and recent developments.
+        Archives of computational methods in engineering, 17(1), 25-76
+        <http://link.springer.com/article/10.1007/s11831-010-9040-7>`_.
+    """
     def __init__(self, dim=2):
         self.radius_scale = 3.0
         self.dim = dim
@@ -270,6 +317,28 @@ class Gaussian(object):
         return -fac * h1 * ( dw*q + w*self.dim )
 
 class QuinticSpline(object):
+    r"""Quintic Spline SPH kernel: [2]_
+
+    .. math::
+
+             W(q) = \ &\sigma_5\left[ (3-q)^5 - 6(2-q)^5 + 15(1-q)^5 \right], \ & \textrm{for} \ 0\leq q \leq 1,\\
+                  = \ &\sigma_5\left[ (3-q)^5 - 6(2-q)^5 \right], & \textrm{for} \ 1 \leq q \leq 2,\\
+                  = \ &\sigma_5 \ (3-5)^5 , & \textrm{for} \ 2 \leq q \leq 3,\\
+                  = \ & 0, & \textrm{for} \ q>3,\\
+
+    where :math:`\sigma_3` is a dimensional normalizing factor for the
+    quintic spline function given by:
+
+    .. math::
+             \sigma_5  = \ & \frac{120}{h^1}, & \textrm{for dim=1}, \\
+             \sigma_5  = \ & \frac{7}{478\pi h^2}, \ & \textrm{for dim=2}, \\
+             \sigma_5  = \ & \frac{3}{359\pi h^3}, & \textrm{for dim=3}. \\
+
+    Raises
+    ------
+    NotImplementedError
+        Quintic spline currently only supports 2D kernels.
+    """
     def __init__(self, dim=2):
         self.radius_scale = 3.0
         if dim != 2:
