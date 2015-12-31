7627a5e1e2af0d8ff21626a4e2be4fefa9401cc6
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2015-12-31 15:03:12 +0530

    Add experimental data for dam break.
    The data is digitized from a couple of papers.  This is used to
    benchmark the 2D dam break results and a post_process method has been
    added.

--- pysph/examples/dam_break_2d.py
+++ pysph/examples/dam_break_2d.py
@@ -5,10 +5,10 @@ flows", Moncho Gomez-Gesteira, Benedict D Rogers, Robert A, Dalrymple and Alex
 J.C Crespo, Journal of Hydraulic Research, Vol 48, Extra Issue (2010), pp
 6-27. DOI:10.1080/00221686.2010.9641242
 
-By default the simulation runs for 2.5 seconds of simulation time.
 """
 
-import numpy
+import os
+import numpy as np
 
 from pysph.examples._db_geometry import DamBreak2DGeometry
 
@@ -30,10 +30,8 @@ container_width = 4.0
 nboundary_layers = 2
 #h = 0.0156
 h = 0.039
-hdx = 1.5
-dx = dy = h/hdx
 ro = 1000.0
-co = 10.0 * numpy.sqrt(2*9.81*fluid_column_height)
+co = 10.0 * np.sqrt(2*9.81*fluid_column_height)
 gamma = 7.0
 alpha = 0.1
 beta = 0.0
@@ -41,18 +39,33 @@ B = co*co*ro/gamma
 p0 = 1000.0
 
 class DamBreak2D(Application):
+    def add_user_options(self, group):
+        self.hdx = 1.3
+        group.add_option(
+            "--h-factor", action="store", type=float, dest="h_factor",
+            default=1.0,
+            help="Divide default h by this factor to change resolution"
+        )
+
+    def consume_user_options(self):
+        self.h = h/self.options.h_factor
+        print("Using h = %f"%self.h)
+        self.hdx = self.hdx
+        self.dx = self.h/self.hdx
+
     def create_particles(self):
         geom = DamBreak2DGeometry(
             container_width=container_width, container_height=container_height,
             fluid_column_height=fluid_column_height,
-            fluid_column_width=fluid_column_width, dx=dx, dy=dy,
+            fluid_column_width=fluid_column_width, dx=self.dx, dy=self.dx,
             nboundary_layers=1, ro=ro, co=co,
             with_obstacle=False,
-            beta=2.0, nfluid_offset=1, hdx=hdx)
+            beta=2.0, nfluid_offset=1, hdx=self.hdx)
         return geom.create_particles()
 
     def create_solver(self):
-        dt = 1e-4
+        dt = 0.125*self.h/co
+        print("dt = %f"%dt)
         tf = 2.5
 
         dim = 2
@@ -103,12 +116,43 @@ class DamBreak2D(Application):
 
             # smoothing length update
             Group( equations=[
-                    UpdateSmoothingLengthFerrari(dest='fluid', sources=None, hdx=1.2, dim=2)
+                    UpdateSmoothingLengthFerrari(dest='fluid', sources=None, hdx=self.hdx, dim=2)
                     ], real=True ),
             ]
         return equations
 
+    def post_process(self, info_fname):
+        info = self.read_info(info_fname)
+        if len(self.output_files) == 0:
+            return
+
+        from pysph.solver.utils import iter_output
+        t, x_max = [], []
+        factor = np.sqrt(2.0*9.81/fluid_column_width)
+        files = self.output_files
+        for sd, array in iter_output(files, 'fluid'):
+            t.append(sd['t']*factor)
+            x = array.get('x')
+            x_max.append(x.max())
+
+        t, x_max = list(map(np.asarray, (t, x_max)))
+        fname = os.path.join(self.output_dir, 'results.npz')
+        np.savez(fname, t=t, x_max=x_max)
+
+        import matplotlib
+        matplotlib.use('Agg')
+        from matplotlib import pyplot as plt
+        from pysph.examples import db_exp_data as dbd
+        plt.plot(t, x_max, label='Computed')
+        te, xe = dbd.get_koshizuka_oka_data()
+        plt.plot(te, xe, 'o', label='Koshizuka & Oka (1996)')
+        plt.xlim(0, 0.7*factor); plt.ylim(0, 4.5)
+        plt.xlabel('$T$'); plt.ylabel('$Z/L$')
+        plt.legend(loc='upper left')
+        plt.savefig(os.path.join(self.output_dir, 'x_vs_t.png'), dpi=300)
+        plt.close()
 
 if __name__ == '__main__':
     app = DamBreak2D()
     app.run()
+    app.post_process(app.info_filename)
