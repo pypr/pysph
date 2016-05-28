b56a1a3e009a3c81d1ac91dc29847101f3a380e7
Aditya Bhosale <adityapb1546@gmail.com>
2016-05-28 23:34:25 +0530

    Fix dependencies for nnps_base

--- setup.py
+++ setup.py
@@ -263,6 +263,10 @@ def get_basic_extensions():
         Extension(
             name="pysph.base.nnps_base",
             sources=["pysph/base/nnps_base.pyx"],
+            depends=get_deps(
+                "pyzoltan/core/carray", "pysph/base/point",
+                "pysph/base/particle_array",
+            ),
             include_dirs=include_dirs,
             extra_compile_args=extra_compile_args + openmp_compile_args,
             extra_link_args=openmp_link_args,
