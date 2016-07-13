89b333b5730075c0f6d8967e29571476a2e25f96
Anshumankumar <anshu266man@gmail.com>
2016-07-13 10:15:05 +0530

    wqwqAdding the mpm scheme for blast wave

--- pysph/examples/gas_dynamics/blastwave.py
+++ pysph/examples/gas_dynamics/blastwave.py
@@ -27,7 +27,6 @@ class Blastwave(ShockTubeSetup):
     def create_particles(self):
         lng = numpy.zeros(1, dtype=float)
         consts ={ 'lng': lng}
-
         return self.generate_particles(xmin = -0.5, xmax=0.5, dxl=dxl, dxr=dxr,
                 m=dxl, pl=1000.0, pr=0.01, h0=h0, bx=0.03,
                 gamma1=gamma1, constants=consts)
@@ -36,10 +35,17 @@ class Blastwave(ShockTubeSetup):
         self.dt = dt
         self.tf = tf
         adke = ADKEScheme(
-            fluids=['fluid'], solids=['boundary'], dim=dim, gamma=gamma, alpha=1,
-            beta=1, k=1.0, eps=0.5, g1=0.2, g2=0.4)
+                fluids=['fluid'], solids=['boundary'], dim=dim, gamma=gamma, alpha=1,
+                beta=1, k=1.0, eps=0.5, g1=0.2, g2=0.4)
 
-        s = SchemeChooser(default='adke', adke=adke)
+        # Running this will need to implement boundary condtion first.  
+        mpm = GasDScheme(  
+                fluids=['fluid'], dim=dim, gamma=gamma,
+                kernel_factor=kernel_factor, alpha1=1.0, alpha2=0.1,
+                beta=2.0, update_alpha1=True, update_alpha2=True
+                )
+
+        s = SchemeChooser(default='adke', adke=adke, mpm=mpm)
         return s
 
 
