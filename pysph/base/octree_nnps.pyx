c112eff1d7519c363764df0e11a43d71360337cb
adityapb <adityapb1546@gmail.com>
2016-09-14 21:33:35 +0530

    Reorganize OctreeNNPS

--- pysph/base/octree_nnps.pyx
+++ pysph/base/octree_nnps.pyx
@@ -53,7 +53,10 @@ cdef inline void delete_tree(OctreeNode* root) nogil:
             delete_tree(temp[i])
 
 
+#############################################################################
 cdef class OctreeNNPS(NNPS):
+    """Nearest neighbor search using Octree.
+    """
     def __init__(self, int dim, list particles, double radius_scale = 2.0,
             int ghost_layers = 1, domain=None, bint fixed_h = False,
             bint cache = False, bint sort_gids = False, int leaf_max_particles = 10):
@@ -84,13 +87,96 @@ cdef class OctreeNNPS(NNPS):
             delete_tree(self.root[i])
         free(self.root)
 
-    @cython.cdivision(True)
-    cdef void build_tree(self, NNPSParticleArrayWrapper pa, UIntArray indices,
-            double* xmin, double length, OctreeNode* root = NULL):
-        """Build octree"""
 
-        if root == NULL:
-            root = self.root[self.src_index]
+    #### Public protocol ################################################
+
+    cpdef set_context(self, int src_index, int dst_index):
+        """Set context for nearest neighbor searches.
+
+        Parameters
+        ----------
+        src_index: int
+            Index in the list of particle arrays to which the neighbors belong
+
+        dst_index: int
+            Index in the list of particle arrays to which the query point belongs
+
+        """
+
+        NNPS.set_context(self, src_index, dst_index)
+        self.current_tree = self.root[src_index]
+
+        self.dst = <NNPSParticleArrayWrapper> self.pa_wrappers[dst_index]
+        self.src = <NNPSParticleArrayWrapper> self.pa_wrappers[src_index]
+
+    cdef void find_nearest_neighbors(self, size_t d_idx, UIntArray nbrs) nogil:
+        """Low level, high-performance non-gil method to find neighbors.
+        This requires that `set_context()` be called beforehand.  This method
+        does not reset the neighbors array before it appends the
+        neighbors to it.
+
+        """
+        cdef double* dst_x_ptr = self.dst.x.data
+        cdef double* dst_y_ptr = self.dst.y.data
+        cdef double* dst_z_ptr = self.dst.z.data
+        cdef double* dst_h_ptr = self.dst.h.data
+
+        cdef double* src_x_ptr = self.src.x.data
+        cdef double* src_y_ptr = self.src.y.data
+        cdef double* src_z_ptr = self.src.z.data
+        cdef double* src_h_ptr = self.src.h.data
+
+        cdef double x = dst_x_ptr[d_idx]
+        cdef double y = dst_y_ptr[d_idx]
+        cdef double z = dst_z_ptr[d_idx]
+        cdef double h = dst_h_ptr[d_idx]
+
+        cdef u_int* s_gid = self.src.gid.data
+        cdef int orig_length = nbrs.length
+
+        self._get_neighbors(x, y, z, h, src_x_ptr, src_y_ptr, src_z_ptr,
+                src_h_ptr, nbrs, self.root[self.src_index])
+
+        if self.sort_gids:
+            self._sort_neighbors(
+                &nbrs.data[orig_length], nbrs.length - orig_length, s_gid
+            )
+
+    cpdef get_nearest_particles_no_cache(self, int src_index, int dst_index,
+            size_t d_idx, UIntArray nbrs, bint prealloc):
+        """Find nearest neighbors for particle id 'd_idx' without cache
+
+        Parameters
+        ----------
+        src_index: int
+            Index in the list of particle arrays to which the neighbors belong
+
+        dst_index: int
+            Index in the list of particle arrays to which the query point belongs
+
+        d_idx: size_t
+            Index of the query point in the destination particle array
+
+        nbrs: UIntArray
+            Array to be populated by nearest neighbors of 'd_idx'
+
+        """
+        self.set_context(src_index, dst_index)
+
+        if prealloc:
+            nbrs.length = 0
+        else:
+            nbrs.c_reset()
+
+        self.find_nearest_neighbors(d_idx, nbrs)
+
+
+    #### Private protocol ################################################
+
+    @cython.cdivision(True)
+    cdef void _build_tree(self, NNPSParticleArrayWrapper pa, UIntArray indices,
+            double* xmin, double length, OctreeNode* root):
+        """Build octree"""
 
         cdef double* src_x_ptr = pa.x.data
         cdef double* src_y_ptr = pa.y.data
@@ -142,35 +228,15 @@ cdef class OctreeNNPS(NNPS):
                     root.children[i][j][k] = new_node(xmin_new, (length/2)*(1 + 2*EPS),
                             hmax=hmax_children[k+2*j+4*i])
 
-                    self.build_tree(pa, <UIntArray>new_indices[k+2*j+4*i],
+                    self._build_tree(pa, <UIntArray>new_indices[k+2*j+4*i],
                             xmin_new, (length/2)*(1 + 2*EPS), root.children[i][j][k])
 
-    cpdef set_context(self, int src_index, int dst_index):
-        """Set context for nearest neighbor searches.
-
-        Parameters
-        ----------
-        src_index: int
-            Index in the list of particle arrays to which the neighbors belong
-
-        dst_index: int
-            Index in the list of particle arrays to which the query point belongs
-
-        """
-
-        NNPS.set_context(self, src_index, dst_index)
-        self.current_tree = self.root[src_index]
-
-        self.dst = <NNPSParticleArrayWrapper> self.pa_wrappers[dst_index]
-        self.src = <NNPSParticleArrayWrapper> self.pa_wrappers[src_index]
 
     @cython.cdivision(True)
     cdef void _get_neighbors(self, double q_x, double q_y, double q_z, double q_h,
             double* src_x_ptr, double* src_y_ptr, double* src_z_ptr, double* src_h_ptr,
-            UIntArray nbrs, OctreeNode* root = NULL) nogil:
-        if root == NULL:
-            root = self.current_tree
-
+            UIntArray nbrs, OctreeNode* root) nogil:
+        """Find neighbors recursively"""
         cdef double x_centre = root.xmin[0] + root.length/2
         cdef double y_centre = root.xmin[1] + root.length/2
         cdef double z_centre = root.xmin[2] + root.length/2
@@ -207,66 +273,6 @@ cdef class OctreeNNPS(NNPS):
                             src_x_ptr, src_y_ptr, src_z_ptr, src_h_ptr,
                             nbrs, root.children[i][j][k])
 
-    cdef void find_nearest_neighbors(self, size_t d_idx, UIntArray nbrs) nogil:
-        """Low level, high-performance non-gil method to find neighbors.
-        This requires that `set_context()` be called beforehand.  This method
-        does not reset the neighbors array before it appends the
-        neighbors to it.
-
-        """
-        cdef double* dst_x_ptr = self.dst.x.data
-        cdef double* dst_y_ptr = self.dst.y.data
-        cdef double* dst_z_ptr = self.dst.z.data
-        cdef double* dst_h_ptr = self.dst.h.data
-
-        cdef double* src_x_ptr = self.src.x.data
-        cdef double* src_y_ptr = self.src.y.data
-        cdef double* src_z_ptr = self.src.z.data
-        cdef double* src_h_ptr = self.src.h.data
-
-        cdef double x = dst_x_ptr[d_idx]
-        cdef double y = dst_y_ptr[d_idx]
-        cdef double z = dst_z_ptr[d_idx]
-        cdef double h = dst_h_ptr[d_idx]
-
-        cdef u_int* s_gid = self.src.gid.data
-        cdef int orig_length = nbrs.length
-
-        self._get_neighbors(x, y, z, h, src_x_ptr, src_y_ptr, src_z_ptr,
-                src_h_ptr, nbrs)
-
-        if self.sort_gids:
-            self._sort_neighbors(
-                &nbrs.data[orig_length], nbrs.length - orig_length, s_gid
-            )
-
-    cpdef get_nearest_particles_no_cache(self, int src_index, int dst_index,
-            size_t d_idx, UIntArray nbrs, bint prealloc):
-        """Find nearest neighbors for particle id 'd_idx' without cache
-
-        Parameters
-        ----------
-        src_index: int
-            Index in the list of particle arrays to which the neighbors belong
-
-        dst_index: int
-            Index in the list of particle arrays to which the query point belongs
-
-        d_idx: size_t
-            Index of the query point in the destination particle array
-
-        nbrs: UIntArray
-            Array to be populated by nearest neighbors of 'd_idx'
-
-        """
-        self.set_context(src_index, dst_index)
-
-        if prealloc:
-            nbrs.length = 0
-        else:
-            nbrs.c_reset()
-
-        self.find_nearest_neighbors(d_idx, nbrs)
 
     cpdef _refresh(self):
         cdef double* xmin = self.xmin.data
@@ -296,7 +302,7 @@ cdef class OctreeNNPS(NNPS):
 
         cdef OctreeNode* tree = self.root[pa_index]
 
-        self.build_tree(pa_wrapper, indices, tree.xmin, tree.length,
+        self._build_tree(pa_wrapper, indices, tree.xmin, tree.length,
                 tree)
 
 
