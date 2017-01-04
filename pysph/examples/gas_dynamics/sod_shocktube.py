6368eddf3ae24fb1ec09b4640af8dfdefa569ba1
Mrinalgouda Patil <mpcsdspa@gmail.com>
2017-01-04 18:13:51 +0000

    sod_shocktube.py edited online with Bitbucket

--- pysph/examples/gas_dynamics/sod_shocktube.py
+++ pysph/examples/gas_dynamics/sod_shocktube.py
@@ -63,7 +63,7 @@ class SodShockTube(ShockTubeSetup):
 
         mpm = GasDScheme(
             fluids=['fluid'], solids=[], dim=dim, gamma=gamma,
-            kernel_factor=None, alpha1=1.0, alpha2=0.1,
+            kernel_factor=1.2, alpha1=1.0, alpha2=0.1,
             beta=2.0, update_alpha1=True, update_alpha2=True
         )
         s = SchemeChooser(default='adke', adke=adke, mpm=mpm)
