8112e0754c0ade0e2ee274e8c8bea4935b8091d7
adityapb <adityapb1546@gmail.com>
2017-05-14 20:26:30 -0500

    Remove GPU algorithms from nnps.py

--- pysph/base/nnps.py
+++ pysph/base/nnps.py
@@ -3,15 +3,12 @@ from pysph.base.nnps_base import get_number_of_threads, py_flatten, py_unflatten
 
 from pysph.base.nnps_base import NNPSParticleArrayWrapper, DomainManager, \
         Cell, NeighborCache, NNPSBase, NNPS
-from pysph.base.gpu_nnps import GPUNeighborCache, GPUNNPS, BruteForceNNPS
 from pysph.base.linked_list_nnps import LinkedListNNPS
 from pysph.base.box_sort_nnps import BoxSortNNPS, DictBoxSortNNPS
 from pysph.base.spatial_hash_nnps import SpatialHashNNPS, ExtendedSpatialHashNNPS
 from pysph.base.cell_indexing_nnps import CellIndexingNNPS
 from pysph.base.z_order_nnps import ZOrderNNPS
-from pysph.base.z_order_gpu_nnps import ZOrderGPUNNPS
 from pysph.base.stratified_hash_nnps import StratifiedHashNNPS
 from pysph.base.stratified_sfc_nnps import StratifiedSFCNNPS
-from pysph.base.stratified_sfc_gpu_nnps import StratifiedSFCGPUNNPS
 from pysph.base.octree_nnps import OctreeNNPS, CompressedOctreeNNPS
 
