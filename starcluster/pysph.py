76f5c517393a7ec56511b3a240bdee8bb69efcc0
ragnar <rahulgovind517@gmail.com>
2017-08-24 13:46:55 +0530

    Documentation updated. Replaced update-alternatives with set of startup scripts.

--- starcluster/pysph.py
+++ starcluster/pysph.py
@@ -3,49 +3,56 @@ from starcluster.logger import log
 
 
 class PySPHInstallerBase(DefaultClusterSetup):
-    # PYSPH_PROFILE = '/etc/profile.d/pysph.sh'
+    PYSPH_PROFILE = "/etc/profile.d/pysph.sh"
     PYSPH_HOSTS = "/home/pysph/PYSPH_HOSTS"
+    PYSPH_USER = "pysph"
 
     def _create_env(self, master):
         master.ssh.execute(
-            r"""if [ ! -d '/home/pysph/pysph_env' ]; then
-            mkdir '/home/pysph/pysph_env' && virtualenv /home/pysph/pysph_env;
-            fi""")
+            r"""
+            echo $HOME
+            if [ ! -d ~/pysph_env ]; then
+            mkdir ~/pysph_env &&
+            virtualenv --system-site-packages ~/pysph_env;
+            fi
+            """
+        )
 
     def _install_pysph(self, master):
         commands = r"""
-          source '/home/pysph/pysph_env/bin/activate'
+          . ~/pysph_env/bin/activate
           if ! python -c "import pysph" &> /dev/null; then
           export USE_TRILINOS=1
           export ZOLTAN_INCLUDE=/usr/include/trilinos
           export ZOLTAN_LIBRARY=/usr/lib/x86_64-linux-gnu
-          cd /home/pysph &&
+          cd ~ &&
           git clone https://github.com/pypr/pysph &&
           cd pysph &&
-          pip install -r requirements.txt &&
-          pip install mpi4py &&
           python setup.py install
-          fi"""
+          fi
+          """
         master.ssh.execute(commands)
 
-    def _configure_python(self, node):
-        node.ssh.execute(
-            r"""sudo update-alternatives --install
-            /usr/bin/python python /home/pysph/pysph_env/bin/python2.7 1""")
+    def _configure_profile(self, node):
+        pysph_profile = node.ssh.remote_file(self.PYSPH_PROFILE, 'w')
+        pysph_profile.write("test -e ~/.bashrc && . ~/.bashrc")
+        pysph_profile.close()
 
 
 class PySPHInstaller(PySPHInstallerBase):
     def run(self, nodes, master, user, user_shell, volumes):
         aliases = [n.alias for n in nodes]
 
-        master.ssh.switch_user("pysph")
+        log.info("Configuring PYSPH Profile")
+        for node in nodes:
+            self.pool.simple_job(self._configure_profile,
+                                 (node,))
+        self.pool.wait(len(nodes))
+
+        master.ssh.switch_user(self.PYSPH_USER)
         log.info("Creating virtual environment")
         self._create_env(master)
-
-        for node in nodes:
-            log.info("Updating python link on %s" % node.alias)
-            self.pool.simple_job(self._configure_python, (node,))
-        self.pool.wait(len(nodes))
+        master.ssh.execute("echo '. ~/pysph_env/bin/activate' > ~/.bashrc")
 
         log.info("Installing PySPH")
         self._install_pysph(master)
@@ -55,9 +62,10 @@ class PySPHInstaller(PySPHInstallerBase):
         pysph_hosts.write('\n'.join(aliases) + '\n')
 
     def on_add_node(self, new_node, nodes, master, user, user_shell, volumes):
-        log.info("Updating python link on %s" % new_node.alias)
-        self._configure_python(new_node)
+        log.info("Configuring PYSPH Profile")
+        self._configure_profile(new_node)
 
+        master.ssh.switch_user(self.PYSPH_USER)
         log.info("Adding %s to PYSPH hosts file" % new_node.alias)
         pysph_hosts = master.ssh.remote_file(self.PYSPH_HOSTS, 'a')
         pysph_hosts.write(new_node.alias + '\n')
@@ -65,5 +73,6 @@ class PySPHInstaller(PySPHInstallerBase):
 
     def on_remove_node(self, remove_node, nodes, master,
                        user, user_shell, volumes):
+        master.switch_user(self.PYSPH_USER)
         log.info("Removing %s from PYSPH hosts file" % remove_node.alias)
         master.ssh.remove_lines_from_file(self.PYSPH_HOSTS, remove_node.alias)
