37b8e0ee0acf665d6dca26615f96ffef4dfed9b0
Suraj Sanka <sankasuraj@gmail.com>
2017-08-24 06:01:54 +0530

    added beach geometry function

--- pysph/tools/geometry.py
+++ pysph/tools/geometry.py
@@ -259,15 +259,15 @@ def get_2d_tank(dx=0.001, base_center=np.array([0.0, 0.0]), length=1.0,
     """
 
     base = np.arange(-length / 2., length / 2. + dx, dx) * (1.0 + 0.0j)
-    left_wall = np.arange(0.0, height + dx, dx) * (1.0j) - length / 2.
-    right_wall = np.arange(0.0, height + dx, dx) * (1.0j) + length / 2.
+    left_wall = np.arange(dx, height + dx, dx) * (1.0j) - length / 2.
+    right_wall = np.arange(dx, height + dx, dx) * (1.0j) + length / 2.
     particles = np.concatenate([left_wall, base, right_wall])
     x = particles.real
     y = particles.imag
     value = 1 if outside else -1
     for i in range(1, num_layers):
         x1, y1 = get_2d_tank(dx, np.array(
-            [0.0, -value * i * dx]), length + 2 * i * value * dx,
+            [0.0, -value * i * dx]), length + 2.0 * i * value * dx,
             height + i * value * dx)
         x = np.concatenate([x, x1])
         y = np.concatenate([y, y1])
@@ -672,6 +672,59 @@ def get_naca_wing(dx=0.01, airfoil='0012', span=1.0, chord=1.0):
     elif len(airfoil) == 5:
         x, y = get_5digit_naca_airfoil(dx, airfoil, chord)
     return extrude(x, y, dx, span)
+
+
+def get_beach_geometry_2d(dx=0.1, l=3.0, h=1.0, flat_l=1.0, angle=45.0,
+                       num_layers=3):
+    """
+    Generates a beach like geometry which is commonly used for simulations
+    related to SPHysics.
+
+    Parameters
+    ----------
+    dx : Spacing between the particles
+    l : Total length of the beach
+    h : Height of the wall used at the beach position
+    flat_l : Length of the flat part
+    angle : Angle of the inclined part
+    num_layers : number of layers
+
+    Returns
+    -------
+    x : 1d numpy array with x coordinates of the beach
+    y : 1d numpy array with y coordinates of the beach
+    x4 : 1d numpy array with x coordinates of the obstacle
+    y4 : 1d numpy array with y coordinates of the obstacle
+    """
+
+    theta = np.pi * angle / 180.0
+    x1, y1 = get_2d_wall(dx, np.array([(flat_l + dx) / 2.0, 0.]), flat_l - dx,
+                         num_layers, False)
+    x2 = np.arange(flat_l - l, 0.0, dx * np.cos(theta))
+    h2 = (l - flat_l) * np.tan(theta)
+    y2_layer = x2 * np.tan(-theta)
+    x2 = np.tile(x2, num_layers)
+    y2 = []
+    for i in range(num_layers):
+        y2.append(y2_layer - i * dx)
+    y2 = np.ravel(np.array(y2))
+    y3 = np.arange(h2 + dx, h + h2, dx)
+    x3_layer = np.ones_like(y3) * (flat_l - l)
+    y3 = np.tile(y3, num_layers)
+    x3 = []
+    for i in range(num_layers):
+        x3.append(x3_layer - i * dx)
+    x3 = np.ravel(np.array(x3))
+    x = np.concatenate([x1, x2, x3])
+    y = np.concatenate([y1, y2, y3])
+    y4 = np.arange(dx, 2.0 * h, dx)
+    x4_layer = np.ones_like(y4) * flat_l
+    y4 = np.tile(y4, num_layers)
+    x4 = []
+    for i in range(num_layers):
+        x4.append(x4_layer + i * dx)
+    x4 = np.ravel(np.array(x4))
+    return x, y, x4, y4
 
 
 def find_overlap_particles(fluid_parray, solid_parray, dx_solid, dim=3):
