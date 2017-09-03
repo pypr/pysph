ad700aeb1fe3305753c31376d219a91b61cb699c
Suraj Sanka <sankasuraj@gmail.com>
2017-09-03 18:27:23 +0530

    Added user options to case2

--- pysph/examples/sphysics/case2.py
+++ pysph/examples/sphysics/case2.py
@@ -3,11 +3,9 @@ SPHysics case2 - dambreak on wet surface (40 minutes)
 """
 
 from pysph.base.kernels import CubicSpline
-from pysph.solver.solver import Solver
 from pysph.solver.application import Application
 
 from pysph.sph.integrator_step import WCSPHStep
-from pysph.sph.integrator import Integrator
 from pysph.base.utils import get_particle_array
 
 import numpy as np
@@ -45,39 +43,53 @@ def get_dam_geometry(dx_tank=0.03, dx_fluid=0.03, r_tank=100.0, h_f=2.0,
 
 class Dambreak_2D(Application):
 
+    def add_user_options(self, group):
+        group.add_argument(
+            "--hdx", action="store", type=float, dest="hdx", default=1.3,
+            help="h/dx value used in SPH to change the smoothness")
+        group.add_argument(
+            "--dx", action="store", type=float, dest="dx", default=0.005,
+            help="spacing between the particles")
+
+    def consume_user_options(self):
+        self.hdx = self.options.hdx
+        self.dx = self.options.dx
+        self.h0 = self.hdx * self.dx
+        self.dt = 0.15 * self.h0 / co
+
     def create_particles(self):
-        fluid, dam = get_dam_geometry(dx_s, dx_f, hdx=hdx, h_f=h_fluid,
-                                      h_f2=h_fluid2, r_fluid=ro, r_tank=ro,
-                                      l_f=l_fluid, l_tank=l_dam, h_tank=h_dam)
+        fluid, dam = get_dam_geometry(self.dx, self.dx, hdx=self.hdx,
+                                      h_f=h_fluid, h_f2=h_fluid2, r_fluid=ro,
+                                      r_tank=ro, l_f=l_fluid, l_tank=l_dam,
+                                      h_tank=h_dam)
         self.scheme.setup_properties([fluid, dam])
-        if self.options.scheme == 'edac':
-            for p in ['rho0', 'arho']:
-                fluid.add_property(p)
-            fluid.rho0[:] = ro
-            fluid.add_output_arrays(['rho0', 'arho'])
         particles = [fluid, dam]
         return particles
 
-    def create_solver(self):
-        kernel = CubicSpline(dim=2)
-        integrator = Integrator(fluid=WCSPHStep())
-
-        solver = Solver(kernel=kernel, dim=2, integrator=integrator,
-                        tf=10.0, dt=dt, adaptive_timestep=False,
-                        output_at_times=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
-                                         8.0, 9.0, 10.0])
-        return solver
-
     def create_scheme(self):
         aha = AdamiHuAdamsScheme(['fluid'], ['dam'], dim=2, rho0=ro, c0=co,
-                                 alpha=alp, gy=-9.81, nu=0.0, h0=h0,
+                                 alpha=alp, gy=-9.81, nu=0.0, h0=0.005,
                                  gamma=1.0)
-        wcsph = WCSPHScheme(['fluid'], ['dam'], dim=2, rho0=ro, c0=co, h0=h0,
-                            hdx=hdx, hg_correction=True, gy=-9.81, alpha=alp,
-                            gamma=gamma, update_h=True)
+        wcsph = WCSPHScheme(['fluid'], ['dam'], dim=2, rho0=ro, c0=co,
+                            h0=0.005, hdx=1.3, hg_correction=True, gy=-9.81,
+                            alpha=alp, gamma=gamma, update_h=True)
         edac = EDACScheme(['fluid'], ['dam'], dim=2, rho0=ro, c0=co, gy=-9.81,
-                          alpha=alp, nu=0.0, h=h0, clamp_p=True)
-        return SchemeChooser(default='wcsph', wcsph=wcsph, aha=aha, edac=edac)
+                          alpha=alp, nu=0.0, h=0.005, clamp_p=True)
+        s = SchemeChooser(default='wcsph', wcsph=wcsph, aha=aha, edac=edac)
+        step = dict(fluid=WCSPHStep())
+        s.configure_solver(kernel=CubicSpline(dim=2), dt=0.0001,
+                           adaptive_timestep=False, extra_steppers=step)
+        return s
+
+    def configure_scheme(self):
+        if self.options.scheme == 'wcsph':
+            self.scheme.configure(h0=self.h0, hdx=self.hdx)
+        elif self.options.scheme == 'aha':
+            self.scheme.configure(h0=self.h0)
+        elif self.options.scheme == 'edac':
+            self.scheme.configure(h=self.h0)
+        kw = dict(dt=self.dt, tf=10.0)
+        self.scheme.configure_solver(**kw)
 
 
 if __name__ == '__main__':
@@ -90,10 +102,5 @@ if __name__ == '__main__':
     alp = 0.1
     ro = 100.0
     co = 10.0 * np.sqrt(2.0 * 9.81 * h_fluid)
-    dx_s = 0.005
-    dx_f = 0.005
-    hdx = 1.3
-    h0 = max(dx_s, dx_f) * hdx
-    dt = 0.15 * h0 / co
     app = Dambreak_2D()
     app.run()
