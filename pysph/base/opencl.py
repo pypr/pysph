798349e5875ca1833071bc17102ca8c18febe85f
Aditya Bhosale <adityab@iitb.ac.in>
2017-10-09 01:49:24 +0530

    Fix print_profile

--- pysph/base/opencl.py
+++ pysph/base/opencl.py
@@ -55,6 +55,7 @@ def print_profile():
                            reverse=True)
     if len(_profile_info) == 0:
         print("No profile information available")
+        return
     print("{:<30} {:<30}".format('Kernel', 'Time'))
     for kernel, time in _profile_info:
         print("{:<30} {:<30}".format(kernel, time))
