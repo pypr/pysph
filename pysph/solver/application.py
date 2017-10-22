c96229567113d8230908110f84cb9928214db58b
ragnar <rahulgovind517@gmail.com>
2017-10-22 18:33:58 +0530

    Add OpenMP schedule argument and handling for the same

--- pysph/solver/application.py
+++ pysph/solver/application.py
@@ -387,6 +387,16 @@ class Application(object):
             default=None,
             help="Do not use OpenMP to run the "
             "simulation using multiple cores.")
+
+        # --omp-schedule
+        parser.add_argument(
+            "--omp-schedule",
+            action="store",
+            dest="omp_schedule",
+            default="dynamic,64",
+            help="""Schedule how loop iterations 
+            are divided amongst multiple threads""")
+        
         # --opencl
         parser.add_argument(
             "--opencl",
@@ -814,6 +824,8 @@ class Application(object):
         # Setup configuration options.
         if options.with_openmp is not None:
             get_config().use_openmp = options.with_openmp
+        if options.omp_schedule is not None:
+            get_config().set_omp_schedule(options.omp_schedule)
         if options.with_opencl:
             get_config().use_opencl = True
         if options.use_double:
