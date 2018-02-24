0f2fc6e97918d6af639b231cdcd38e17ffa5df48
Suraj Sanka <sankasuraj@gmail.com>
2018-02-24 07:12:16 +0530

    Added more tests for gj_Solve

--- pysph/sph/tests/test_gjsolve.py
+++ pysph/sph/tests/test_gjsolve.py
@@ -5,18 +5,74 @@ import unittest
 
 class TestGjSolve(unittest.TestCase):
 
-    def test_gj_solve(self):
-        for i in range(10):
-            n = np.random.choice([3, 4])
-            mat = 10.0 * (np.random.random_sample((n, n)))
-            b = [0.0] * n
-            b[0] = 1.0
-            result = [0.0] * n
-            gj_Solve(np.ravel(mat), b, n, result)
-            mat = np.matrix(mat)
-            new_b = mat * np.transpose(np.matrix(result))
-            new_b = np.ravel(np.array(new_b))
-            assert np.allclose(new_b, np.array(b))
+    def testGeneralMatrix(self):
+        n = 4
+        mat = [[0.02, 0.01, 0., 0.], [1., 2., 1., 0.], [0., 1., 2., 1.],
+               [0., 0., 100., 200.]]
+        b = [0.02, 1., 4., 800.]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
+
+    def testBandMatrix(self):
+        n = 3
+        mat = [[1., -2., 0.], [1., -1., 3.], [2., 5., 0.]]
+        b = [-3., 1., 0.5]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
+
+    def testDenseMatrix(self):
+        n = 3
+        mat = [[0.96, 4.6, -3.7], [2.7, 4.3, -0.67], [0.9, 0., -5.]]
+        b = [2.4, 3.6, -5.8]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
+
+    def testTriDiagonalMatrix(self):
+        n = 4
+        mat = [[-2., 1., 0., 0.], [1., -2., 1., 0.], [0., 1., -2., 0.],
+               [0., 0., 1., -2.]]
+        b = [-1., 0., 0., -5.]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
+
+    def testSymmetricMatrix(self):
+        n = 3
+        mat = [[0.96, 4.6, -3.7], [4.6, 4.3, -0.67], [-3.7, -0.67, -5.]]
+        b = [2.4, 3.6, -5.8]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
+
+    def testSymmetricPositiveDefiniteMatrix(self):
+        n = 4
+        mat = [[1., 1., 4., -1.], [1., 5., 0., -1.], [4., 0., 21., -4.],
+               [-1., -1., -4., 10.]]
+        b = [2.4, 3.6, -5.8, 0.5]
+        result = [0.0] * n
+        gj_Solve(np.ravel(mat), b, n, result)
+        mat = np.matrix(mat)
+        new_b = mat * np.transpose(np.matrix(result))
+        new_b = np.ravel(np.array(new_b))
+        assert np.allclose(new_b, np.array(b))
 
 
 if __name__ == '__main__':
