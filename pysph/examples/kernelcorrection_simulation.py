55a0932d6e03337803bd6c44eedd26e9f2a60b6a
Suraj Sanka <sankasuraj@gmail.com>
2018-04-11 23:53:47 +0530

    Changes in import

--- pysph/examples/kernelcorrection_simulation.py
+++ pysph/examples/kernelcorrection_simulation.py
@@ -15,8 +15,9 @@ from pysph.sph.equation import Group
 from pysph.sph.scheme import WCSPHScheme
 from pysph.tools.geometry import remove_overlap_particles, rotate
 from pysph.tools.geometry import get_2d_tank, get_2d_block
-from kernel_correction import GradientCorrectionPreStep, GradientCorrection
-from kernel_correction import MixedKernelCorrectionPreStep
+from pysph.sph.wc.kernel_correction import (GradientCorrectionPreStep,
+                                            GradientCorrection,
+                                            MixedKernelCorrectionPreStep)
 
 
 def get_dam_geometry(dx_tank=0.03, dx_fluid=0.03, r_tank=100.0, h_f=2.0,
@@ -44,7 +45,8 @@ def get_dam_geometry(dx_tank=0.03, dx_fluid=0.03, r_tank=100.0, h_f=2.0,
 class Dambreak2D(Application):
 
     def create_particles(self):
-        fluid, dam = f, d
+        fluid, dam = get_dam_geometry(
+            0.05, 0.05, hdx=1.5, h_f=2.0, r_fluid=100.0, r_tank=100.0)
         leng = len(fluid.x) * 9
         fluid.add_property('cwij')
         dam.add_property('cwij')
@@ -54,9 +56,10 @@ class Dambreak2D(Application):
         return particles
 
     def create_scheme(self):
-        return WCSPHScheme(['fluid'], ['dam'], dim=2, rho0=ro, c0=co, h0=h0,
-                           hdx=hd, hg_correction=True, gy=-9.81, alpha=alp,
-                           gamma=gamma)
+        co = 10.0 * np.sqrt(2.0 * 9.81 * 2.0)
+        return WCSPHScheme(['fluid'], ['dam'], dim=2, rho0=100.0, c0=co, h0=0.075,
+                           hdx=1.5, hg_correction=True, gy=-9.81, alpha=0.2,
+                           gamma=7.0)
 
     def create_equations(self):
         eqns = self.scheme.get_equations()
@@ -70,21 +73,13 @@ class Dambreak2D(Application):
         return eqns
 
     def configure_scheme(self):
+        co = 10.0 * np.sqrt(2.0 * 9.81 * 2.0)
+        dt = 0.3 * 0.075 / co
         s = self.scheme
         s.configure_solver(kernel=CubicSpline(dim=2), dt=dt, tf=5.0,
                            adaptive_timestep=False)
 
 
 if __name__ == '__main__':
-    h_fluid = 2.0
-    gamma = 7.0
-    alp = 0.2
-    ro = 100.0
-    co = 10.0 * np.sqrt(2.0 * 9.81 * h_fluid)
-    dx = 0.05
-    hd = 1.5
-    h0 = dx * hd
-    dt = 0.3 * h0 / co
-    f, d = get_dam_geometry(dx, dx, hdx=hd, h_f=h_fluid, r_fluid=ro, r_tank=ro)
     app = Dambreak2D()
     app.run()
