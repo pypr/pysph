1b05ef467e6838bdea56f7ab417f4f1979e35936
adityapb <adityapb1546@gmail.com>
2018-08-26 15:10:11 +0530

    Check if kernel goes out of bound cpy

--- pysph/cpy/tests/test_low_level.py
+++ pysph/cpy/tests/test_low_level.py
@@ -17,11 +17,12 @@ class TestKernel(unittest.TestCase):
     def test_simple_kernel_opencl(self):
         importorskip('pyopencl')
         # Given
-        @annotate(gdoublep='x, y', a='float')
-        def knl(x, y, a):
+        @annotate(gdoublep='x, y', a='float', size='int')
+        def knl(x, y, a, size):
             i = declare('int')
             i = GID_0*LDIM_0 + LID_0
-            y[i] = x[i]*a
+            if i < size:
+                y[i] = x[i]*a
 
         x = np.linspace(0, 1, 1000)
         y = np.zeros_like(x)
@@ -30,7 +31,7 @@ class TestKernel(unittest.TestCase):
         # When
         k = Kernel(knl, backend='opencl')
         a = 21.0
-        k(x, y, a)
+        k(x, y, a, 1000)
 
         # Then
         y.pull()
@@ -39,11 +40,12 @@ class TestKernel(unittest.TestCase):
     def test_simple_kernel_cuda(self):
         importorskip('pycuda')
         # Given
-        @annotate(gdoublep='x, y', a='float')
-        def knl(x, y, a):
+        @annotate(gdoublep='x, y', a='float', size='int')
+        def knl(x, y, a, size):
             i = declare('int')
             i = GID_0*LDIM_0 + LID_0
-            y[i] = x[i]*a
+            if i < size:
+                y[i] = x[i]*a
 
         x = np.linspace(0, 1, 1000)
         y = np.zeros_like(x)
@@ -52,7 +54,7 @@ class TestKernel(unittest.TestCase):
         # When
         k = Kernel(knl, backend='cuda')
         a = 21.0
-        k(x, y, a)
+        k(x, y, a, size)
 
         # Then
         y.pull()
