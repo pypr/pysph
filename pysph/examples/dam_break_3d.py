dc478754eb4845112b1ae3bf2f792b90cbc1ad73
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2019-03-23 12:56:34 +0530

    Fix some issues with the dam break 3d example.
    The obstacle was not seen, change number of boundary particles, and use
    slightly higher hdx, also add hdx as a command line parameter.

--- pysph/examples/dam_break_3d.py
+++ pysph/examples/dam_break_3d.py
@@ -21,12 +21,12 @@ tf = 6.0
 
 # parameter to change the resolution
 dx = 0.02
-nboundary_layers = 3
-hdx = 1.2
+nboundary_layers = 1
+hdx = 1.3
 ro = 1000.0
 h0 = dx * hdx
 gamma = 7.0
-alpha = 0.5
+alpha = 0.25
 beta = 0.0
 c0 = 10.0 * np.sqrt(2.0 * 9.81 * 0.55)
 
@@ -37,28 +37,34 @@ class DamBreak3D(Application):
             '--dx', action='store', type=float, dest='dx',  default=dx,
             help='Particle spacing.'
         )
+        group.add_argument(
+            '--hdx', action='store', type=float, dest='hdx',  default=hdx,
+            help='Specify the hdx factor where h = hdx * dx.'
+        )
 
     def consume_user_options(self):
         dx = self.options.dx
         self.dx = dx
+        self.hdx = self.options.hdx
         self.geom = DamBreak3DGeometry(
-            dx=dx, nboundary_layers=nboundary_layers, hdx=hdx, rho0=ro
+            dx=dx, nboundary_layers=nboundary_layers, hdx=self.hdx, rho0=ro
         )
         self.co = 10.0 * self.geom.get_max_speed(g=9.81)
 
     def create_scheme(self):
         s = WCSPHScheme(
-            ['fluid'], ['boundary'], dim=dim, rho0=ro, c0=c0,
+            ['fluid'], ['boundary', 'obstacle'], dim=dim, rho0=ro, c0=c0,
             h0=h0, hdx=hdx, gz=-9.81, alpha=alpha, beta=beta, gamma=gamma,
-            hg_correction=True, tensile_correction=True
+            hg_correction=True, tensile_correction=False
         )
         return s
 
     def configure_scheme(self):
         s = self.scheme
+        hdx = self.hdx
         kernel = WendlandQuintic(dim=dim)
         h0 = self.dx * hdx
-        s.configure(h0=h0)
+        s.configure(h0=h0, hdx=hdx)
         dt = 0.25*h0/(1.1 * self.co)
         s.configure_solver(
             kernel=kernel, integrator_cls=EPECIntegrator, tf=tf, dt=dt,
