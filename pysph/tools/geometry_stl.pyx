7618704aafc6ab4997b88d8c402d4d5959574d6c
amalss18 <amalssebastian@gmail.com>
2019-05-30 12:01:15 +0530

    added funtion to remove exterior points

--- pysph/tools/geometry_stl.pyx
+++ pysph/tools/geometry_stl.pyx
@@ -4,6 +4,8 @@ import numpy as np
 from stl import mesh
 from numpy.linalg import norm
 from cyarray.api import UIntArray
+import time
+from mayavi import mlab
 
 
 class ZeroAreaTriangleException(Exception):
@@ -83,40 +85,13 @@ def _fill_triangle(triangle, h=0.1):
                                st[2, 0], st[2, 1])
 
     s_mesh, t_mesh = s_mesh[mask], t_mesh[mask]
-    st_mesh = np.vstack([s_mesh, t_mesh])
+    # st_mesh = np.vstack([s_mesh, t_mesh])
 
     # Final mesh coordinates generated from parameters s and t
 
     result = np.dot(s_mesh.reshape(-1, 1), a.reshape(-1, 1).T) + \
         np.dot(t_mesh.reshape(-1, 1), b.reshape(-1, 1).T) + p
     return result[:, 0], result[:, 1], result[:, 2]
-
-
-def _get_neighbouring_particles(pa_src, pa_dst, radius_scale):
-    """
-    Parameters
-    ----------
-    pa_src : Source particle array
-    pa_dst : Destination particle array
-    """
-    pa_list = [pa_dst, pa_src]
-    nps = nnps.LinkedListNNPS(dim=3, particles=pa_list,
-                              radius_scale=radius_scale)
-
-    nps.set_context(src_index=1, dst_index=0)
-
-    n = len(pa_dst.x)
-    nbrs = UIntArray()
-    grid_set = set()
-    for i in range(n):
-        nps.get_nearest_particles(1, 0, i, nbrs)
-        neighbours = list(nbrs.get_npy_array())
-        for neighbour in neighbours:
-            grid_set.add(neighbour)
-
-    idx = list(grid_set)
-
-    return pa_src.x[idx], pa_src.y[idx], pa_src.z[idx]
 
 
 def _get_stl_mesh(stl_fname, dx_triangle):
@@ -137,61 +112,196 @@ def _get_stl_mesh(stl_fname, dx_triangle):
     x = np.concatenate(x_list)
     y = np.concatenate(y_list)
     z = np.concatenate(z_list)
-
     return x, y, z
 
 
-def get_stl_surface(stl_fname, dx_sph, h_sph, radius_scale=1.0,
-                    dx_triangle=None):
-    """Generate points to cover surface described by stl file
+def _get_stl_mesh_uniform(stl_fname, dx_triangle):
+    """Interpolates points within triangles in the stl file"""
+    m = mesh.Mesh.from_file(stl_fname)
+    x_list, y_list, z_list = [], [], []
+    for i in range(len(m.vectors)):
+        try:
+            x1, y1, z1 = _fill_triangle(m.vectors[i], dx_triangle)
+            x2, y2, z2 = _get_triangle_sides(m.vectors[i], dx_triangle)
+            x_list.append(np.r_[x1, x2])
+            y_list.append(np.r_[y1, y2])
+            z_list.append(np.r_[z1, z2])
+        except ZeroAreaTriangleException as e:
+            print(e)
+            print("Skipping triangle {}".format(i))
+            continue
+    sizes = []  # contains number of points corresponding to each triangle
+    for i in range(np.shape(x_list)[0]):
+        sizes.append(np.shape(x_list[i])[0])
+    x = np.concatenate(x_list)
+    y = np.concatenate(y_list)
+    z = np.concatenate(z_list)
+    centroids_x = []
+    centroids_y = []
+    centroids_z = []
+    for i in range(np.shape(m.vectors)[0]):
+        centroid = ((m.vectors[i][0]+m.vectors[i][1]+m.vectors[i][2]))/3
+        centroids_x.append(centroid[0])
+        centroids_y.append(centroid[1])
+        centroids_z.append(centroid[2])
+    return x, y, z, centroids_x, centroids_y, centroids_z, sizes
 
-    The function generates a grid with a spacing of dx_sph and keeps points at a
-    distance ``radius_scale * h_sph`` around the surface described by the STL
-    file.
 
-    The algorithm for this is straightforward and consists of the following
-    steps:
-
-    1. Interpolate a set of points over the STL triangles
-    2. Create a grid that covers the entire STL object
-    3. Remove points more than ``h_sph * radius_scale`` distance from a surface
-
+def _get_neighbouring_particles(pa_src, pa_dst, radius_scale, sizes):
+    """
     Parameters
     ----------
-
-    stl_fname : str
-        File name of STL file
-    dx_sph : float
-        Spacing in generated grid points
-    h_sph : float
-        Smoothing length
-    radius_scale : float
-        Kernel radius scale
-    dx_triangle : float, optional
-        By default, dx_triangle = 0.5 * dx_sph
-
-    Returns
-    -------
-
-    x : ndarray
-        1d numpy array with x coordinates of surface grid
-    y : ndarray
-        1d numpy array with y coordinates of surface grid
-    z : ndarray
-        1d numpy array with z coordinates of surface grid
-
-    Raises
-    ------
-
-    PolygonMeshError
-        If polygons in STL file are not all triangles
+    pa_src : Source particle array
+    pa_dst : Destination particle array
     """
+    pa_list = [pa_dst, pa_src]
+    nps = nnps.LinkedListNNPS(dim=3, particles=pa_list,
+                              radius_scale=radius_scale)
+
+    nps.set_context(src_index=1, dst_index=0)
+    sizes = np.array(sizes)
+    sizes[0] = sizes[0]-1
+    for k in range(len(sizes)-1):
+        sizes[k+1] = sizes[k]+sizes[k+1]
+    n = len(pa_dst.x)
+    nbrs = UIntArray()
+    grid_set = []
+    size_count = 0
+    tracker = 0
+    new_sizes = []  # contains new no. of points corresponding to each triangle
+    mulitple_occur = []  # contains the triangles corresponding to every point.
+    # When finding neighbouring particles it is possible that a
+    # neighbouring point obatined might be the nearest neighbour
+    # of mulitple points corresponding to different triangles.
+    for i in range(n):
+        nps.get_nearest_particles(1, 0, i, nbrs)
+        neighbours = list(nbrs.get_npy_array())
+        for neighbour in neighbours:
+            if neighbour in grid_set:
+                if tracker in mulitple_occur[grid_set.index(neighbour)]:
+                    pass
+                else:
+                    mulitple_occur[grid_set.index(neighbour)].append(tracker)
+            else:
+                grid_set.append(neighbour)
+                mulitple_occur.append(list([tracker]))
+                size_count = size_count+1
+        if(i == sizes[tracker]):
+            tracker = tracker+1
+            new_sizes.append(size_count)
+    idx = list(grid_set)
+    return pa_src.x[idx], pa_src.y[idx], pa_src.z[idx], \
+        new_sizes, mulitple_occur
+
+
+def elemental_volume(point, normal, face, radius_scale, dx_sph, centroid,
+                     multiple_occur, centroids_x, centroids_y, centroids_z, u,
+                     v, w):
+    """ Identifies whether a point is within h thickness of it's
+    corresponding side"""
+    mag = norm(normal)
+    normal = normal/mag
+    h = 1.5*radius_scale*dx_sph
+    face_1 = np.zeros_like(face)
+    for k in range(np.shape(face)[1]):
+        for i in range(np.shape(face)[0]):
+            face_1[i][k] = face[i][k]-normal[k]*h
+    centroid_2 = (face_1[0]+face_1[1]+face_1[2])/3
+    for i in multiple_occur:
+        centroid = np.array([centroids_x[i], centroids_y[i], centroids_z[i]])
+        normal = np.array([u[i], v[i], w[i]])
+        if(np.dot(point - centroid, normal) > 0):
+            return True
+
+    if(np.dot(point - centroid, normal) <= 0 and np.dot(point - centroid_2,
+       normal) > 0):
+        return False
+    else:
+        return True
+
+
+def remove_exterior(x, y, z, centroids_x, centroids_y, centroids_z, stl_fname,
+                    sizes, u, v, w, radius_scale, dx_sph, multiple_occur):
+    """ removes points which lie outside the given volume"""
+    remove_point = []
+    my_mesh = mesh.Mesh.from_file(stl_fname)
+    i = 0
+    centroid = np.array([centroids_x[i], centroids_y[i], centroids_z[i]])
+    normal = np.array([u[i], v[i], w[i]])
+    for j in range(len(x)):
+        point = np.array([x[j], y[j], z[j]])
+        if(elemental_volume(point, normal, my_mesh.vectors[i], radius_scale,
+                            dx_sph, centroid, multiple_occur[j], centroids_x,
+                            centroids_y, centroids_z, u, v, w)):
+                remove_point.append(j)
+        if(j == sizes[i]):
+            i = i+1
+            centroid = np.array([centroids_x[i],
+                                centroids_y[i],
+                                centroids_z[i]])
+            normal = np.array([u[i], v[i], w[i]])
+    x = np.delete(x, remove_point)
+    y = np.delete(y, remove_point)
+    z = np.delete(z, remove_point)
+    return x, y, z
+
+
+def fix_normals(x, y, z, centroids_x, centroids_y, centroids_z, stl_fname):
+    """ used to ensure that all normal's are outward normals"""
+    my_mesh = mesh.Mesh.from_file(stl_fname)
+    u, v, w = [], [], []
+    for i in range(np.shape(my_mesh.vectors)[0]):
+        u.append(my_mesh.normals[i][0])
+        v.append(my_mesh.normals[i][1])
+        w.append(my_mesh.normals[i][2])
+    fig1 = mlab.figure(bgcolor=(0, 0, 0))
+    fig1.scene.disable_render = True
+    mlab.points3d(x, y, z, color=(0.1, 0.6, 0.3), opacity=0.3)
+    outline = mlab.outline(line_width=3)
+    outline.outline_mode = "cornered"
+    outline_size = 0.007
+    outline.bounds = (centroids_x[0]-outline_size, centroids_x[0]+outline_size,
+                      centroids_y[0]-outline_size, centroids_y[0]+outline_size,
+                      centroids_z[0]-outline_size, centroids_z[0]+outline_size)
+    normals = mlab.quiver3d(centroids_x, centroids_y, centroids_z,
+                            u, v, w, figure=fig1)
+    centroids = mlab.points3d(centroids_x, centroids_y, centroids_z,
+                              color=(0.5, 0.5, 0.5))
+    fig1.scene.disable_render = False
+    glyph_normals = centroids.glyph.glyph_source.glyph_source.output.points. \
+        to_array()
+
+    def picker_callback(picker):
+        if picker.actor in centroids.actor.actors:
+            centroid_id = picker.point_id//glyph_normals.shape[0]
+            if centroid_id != -1:
+                x, y, z = centroids_x[centroid_id], centroids_y[centroid_id], \
+                    centroids_z[centroid_id]
+                u[centroid_id], v[centroid_id], w[centroid_id] = \
+                    -1*u[centroid_id], -1*v[centroid_id], -1*w[centroid_id]
+                normals.mlab_source.reset(u=u, v=v, w=w)
+                outline.bounds = (x-outline_size, x+outline_size,
+                                  y-outline_size, y+outline_size,
+                                  z-outline_size, z+outline_size)
+    picker = fig1.on_mouse_pick(picker_callback)
+    picker.tolerance = 0.01
+    mlab.title("Click on centroid to invert normal")
+    mlab.show()
+    return u, v, w
+
+
+def get_stl_surface_uniform(stl_fname, dx_sph, h_sph, radius_scale=1.0,
+                            dx_triangle=None):
+    """ Returns coordinates of uniform distribution of
+     particles for given stl"""
     if dx_triangle is None:
         dx_triangle = 0.5 * dx_sph
 
-    x, y, z = _get_stl_mesh(stl_fname, dx_triangle)
+    x, y, z, centroids_x, centroids_y, centroids_z, sizes = \
+        _get_stl_mesh_uniform(stl_fname, dx_triangle)
+    u, v, w = fix_normals(x, y, z, centroids_x, centroids_y, centroids_z,
+                          stl_fname)
     pa_mesh = ParticleArray(name='mesh', x=x, y=y, z=z, h=h_sph)
-
     offset = radius_scale * h_sph
     x_grid, y_grid, z_grid = np.meshgrid(
         np.arange(x.min() - offset, x.max() + offset, dx_sph),
@@ -200,7 +310,22 @@ def get_stl_surface(stl_fname, dx_sph, h_sph, radius_scale=1.0,
     )
 
     pa_grid = ParticleArray(name='grid', x=x_grid, y=y_grid, z=z_grid, h=h_sph)
+    x_grid, y_grid, z_grid, new_sizes, mulitple_occur = \
+        _get_neighbouring_particles(pa_grid, pa_mesh, radius_scale, sizes)
+    xf, yf, zf = remove_exterior(x_grid, y_grid, z_grid,
+                                 centroids_x, centroids_y, centroids_z,
+                                 stl_fname, new_sizes,
+                                 u, v, w,
+                                 radius_scale, dx_sph, mulitple_occur)
+    return xf, yf, zf
 
-    x_grid, y_grid, z_grid = _get_neighbouring_particles(pa_grid, pa_mesh,
-                                                         radius_scale)
-    return x_grid, y_grid, z_grid
+
+def get_stl_surface(stl_fname, dx_sph, h_sph, radius_scale=1.0,
+                    dx_triangle=None):
+    """ Returns coordinates of particle distribution along surface
+    of stl file"""
+    if dx_triangle is None:
+        dx_triangle = 0.5 * dx_sph
+
+    x, y, z = _get_stl_mesh(stl_fname, dx_triangle)
+    return x, y, z
