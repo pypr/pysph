04fe1c750641f4e78bd43f86373d2c03db15d714
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2021-01-13 17:53:59 +0530

    Fix broken test due to API change.

--- pysph/parallel/tests/simple_reduction.py
+++ pysph/parallel/tests/simple_reduction.py
@@ -12,18 +12,6 @@ from pysph.solver.solver import Solver
 from pysph.base.reduce_array import parallel_reduce_array, serial_reduce_array
 
 
-def create_particles():
-    x = np.linspace(0, 10, 10)
-    m = np.ones_like(x)
-    y = np.zeros_like(x)
-    z = np.zeros_like(x)
-    h = np.ones_like(x) * 0.2
-
-    fluid = ParticleArray(name='fluid', x=x, y=y, z=z, m=m, h=h)
-    fluid.add_constant('total_mass', 0.0)
-    return [fluid]
-
-
 class TotalMass(Equation):
     def reduce(self, dst, t, dt):
         m = serial_reduce_array(dst.m, op='sum')
@@ -38,30 +26,44 @@ class DummyStepper(IntegratorStep):
         pass
 
 
+class MyApp(Application):
+    def create_particles(self):
+        x = np.linspace(0, 10, 10)
+        m = np.ones_like(x)
+        y = np.zeros_like(x)
+        z = np.zeros_like(x)
+        h = np.ones_like(x) * 0.2
+
+        fluid = ParticleArray(name='fluid', x=x, y=y, z=z, m=m, h=h)
+        fluid.add_constant('total_mass', 0.0)
+        return [fluid]
+
+    def create_solver(self):
+        dim = 1
+        # Create the kernel
+        kernel = CubicSpline(dim=dim)
+
+        # Create the integrator.
+        integrator = EulerIntegrator(fluid=DummyStepper())
+
+        solver = Solver(kernel=kernel, dim=dim, integrator=integrator)
+        solver.set_time_step(0.1)
+        solver.set_final_time(0.1)
+        # There is no need to write any output as the test below
+        # computes the total mass.
+        solver.set_disable_output(True)
+        return solver
+
+    def create_equations(self):
+        return [TotalMass(dest='fluid', sources=['fluid'])]
+
+
 def main():
     # Create the application.
-    app = Application()
-
-    dim = 1
-    # Create the kernel
-    kernel = CubicSpline(dim=dim)
-
-    # Create the integrator.
-    integrator = EulerIntegrator(fluid=DummyStepper())
-
-    solver = Solver(kernel=kernel, dim=dim, integrator=integrator)
-    solver.set_time_step(0.1)
-    solver.set_final_time(0.1)
-
-    equations = [TotalMass(dest='fluid', sources=['fluid'])]
-    app.setup(
-        solver=solver, equations=equations, particle_factory=create_particles)
-    # There is no need to write any output as the test below
-    # computes the total mass.
-    solver.set_disable_output(True)
+    app = MyApp()
     app.run()
 
-    fluid = solver.particles[0]
+    fluid = app.particles[0]
     err = fluid.total_mass[0] - 10.0
     assert abs(err) < 1e-16, "Error: %s" % err
 
