75a66a03dc9334d5b54704f1a7367760c19d37a7
Navaneet <11260095+nauaneed@users.noreply.github.com>
2022-02-08 00:51:33 +0530

    :construction: (tsph): modified integrator step to predict number density

--- pysph/sph/gas_dynamics/tsph/pec_step.py
+++ pysph/sph/gas_dynamics/tsph/pec_step.py
@@ -1,9 +1,12 @@
 from pysph.sph.integrator_step import IntegratorStep
+
+
 class PECStep(IntegratorStep):
-    """Predictor Corrector integrator for Gas-dynamics"""
+    """Predictor Corrector integrator for Gas-dynamics modified for TSPH"""
+
     def initialize(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z, d_h,
                    d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e, d_e0, d_h0,
-                   d_converged, d_rho, d_rho0, d_alpha1, d_alpha10):
+                   d_converged, d_rho, d_rho0, d_n, d_n0):
         d_x0[d_idx] = d_x[d_idx]
         d_y0[d_idx] = d_y[d_idx]
         d_z0[d_idx] = d_z[d_idx]
@@ -16,18 +19,16 @@ class PECStep(IntegratorStep):
 
         d_h0[d_idx] = d_h[d_idx]
         d_rho0[d_idx] = d_rho[d_idx]
+        d_n0[d_idx] = d_n[d_idx]
 
         # set the converged attribute to 0 at the beginning of a Group
         d_converged[d_idx] = 0
 
-        d_alpha10[d_idx] = d_alpha1[d_idx]
-
     def stage1(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z,
                d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e0, d_e, d_au, d_av,
-               d_aw, d_ae, d_rho, d_rho0, d_arho, d_h, d_h0, d_ah,
-               d_alpha1, d_aalpha1, d_alpha10,
-               dt):
-        dtb2 = 0.5*dt
+               d_aw, d_ae, d_rho, d_rho0, d_arho, d_h, d_h0, d_ah, dt,
+               d_n, d_n0, d_an):
+        dtb2 = 0.5 * dt
 
         d_u[d_idx] = d_u0[d_idx] + dtb2 * d_au[d_idx]
         d_v[d_idx] = d_v0[d_idx] + dtb2 * d_av[d_idx]
@@ -45,16 +46,11 @@ class PECStep(IntegratorStep):
         # will be called at the end of the predictor stage.
         d_h[d_idx] = d_h0[d_idx] + dtb2 * d_ah[d_idx]
         d_rho[d_idx] = d_rho0[d_idx] + dtb2 * d_arho[d_idx]
-
-        # update viscosity coefficients
-        d_alpha1[d_idx] = d_alpha10[d_idx] + dtb2*d_aalpha1[d_idx]
-
+        d_n[d_idx] = d_n0[d_idx] + dtb2 * d_an[d_idx]
 
     def stage2(self, d_idx, d_x0, d_y0, d_z0, d_x, d_y, d_z,
                d_u0, d_v0, d_w0, d_u, d_v, d_w, d_e0, d_e, d_au, d_av,
-               d_alpha1, d_aalpha1, d_alpha10,
                d_aw, d_ae, dt):
-
         d_u[d_idx] = d_u0[d_idx] + dt * d_au[d_idx]
         d_v[d_idx] = d_v0[d_idx] + dt * d_av[d_idx]
         d_w[d_idx] = d_w0[d_idx] + dt * d_aw[d_idx]
@@ -66,5 +62,3 @@ class PECStep(IntegratorStep):
         # Update densities and smoothing lengths from the accelerations
         d_e[d_idx] = d_e0[d_idx] + dt * d_ae[d_idx]
 
-        # update viscosity coefficients
-        d_alpha1[d_idx] = d_alpha10[d_idx] + dt*d_aalpha1[d_idx]
