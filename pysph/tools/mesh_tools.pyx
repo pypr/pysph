279385d2d23c10735897fbfa4f0ce3f7aaebc215
Navaneet <11260095+nauaneed@users.noreply.github.com>
2023-01-07 04:25:54 +0530

    fix(mesh_tools): tests do not fail anymore
    - solved by using len instead of np.shape for list

--- pysph/tools/mesh_tools.pyx
+++ pysph/tools/mesh_tools.pyx
@@ -226,7 +226,7 @@ def get_points_from_mgrid(pa_grid, pa_mesh, x_list, y_list, z_list,
     cdef list selected_points = []
     cdef int counter = 0, l = 0
     # Iterating over each triangle
-    for i in range(np.shape(x_list)[0]):
+    for i in range(len(x_list)):
         prism_normals, prism_points, prism_face_centres = prism(
             normals[i], vectors[i], dx_sph
         )
